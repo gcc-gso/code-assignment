1bc98a400fbabce67ad0656a5051c1ebb52e4be6
Muhammad Ejaz Azeem <mejaz@gso.org.sa>
2018-09-20 09:23:42 +0300

    project setup

---
+++ src/main/java/org/gso/codeassignment/CodeAssignmentApplication.java
@@ -0,0 +1,12 @@
+package org.gso.codeassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeAssignmentApplication.class, args);
+	}
+}
